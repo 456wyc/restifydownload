7956f43659d313ebe17727ef200bbabdd0bc9574
wangyinchuan <456wyc@163.com>
2018-07-15 12:15:11 +0800

    fix header name

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports = function (req, res, next) {
-    res.download = res.download || function (opt, cb) {
-        fs.readFile(opt.path, (err, data) => {
-            if (err) {
-                cb ? cb(err) : {}
-            } else {
-                res.charSet(opt.charSet);
-                res.set({
-                    'content-type': opt.type,
-                    'Content-Disposition': `attachment;filename="${opt.name||new Date().toLocaleString()}"`
-                })
-                res.write(data);
-                res.end();
-                cb ? cb() : {}
-            }
-        })
-    }
-    res.sendFile = res.sendFile || function (data, opt, cb) {
-        res.charSet(opt.charSet);
+  res.download = res.download || function (opt, cb) {
+    fs.readFile(opt.path, (err, data) => {
+      if (err) {
+        cb ? cb(err) : {}
+      } else {
+        res.charSet(opt.charSet)
         res.set({
-            'content-type': opt.type,
-            'Content-Disposition': `attachment;filename="${opt.name||new Date().toLocaleString()}"`
-        });
-        res.write(data);
-        res.end();
+          'Content-Type': opt.type,
+          'Content-Disposition': `attachment;filename="${opt.name||new Date().toLocaleString()}"`
+        })
+        res.write(data)
+        res.end()
         cb ? cb() : {}
-    }
-    next()
+      }
+    })
+  }
+  res.sendFile = res.sendFile || function (data, opt, cb) {
+    res.charSet(opt.charSet)
+    res.set({
+      'Content-Type': opt.type,
+      'Content-Disposition': `attachment;filename="${opt.name||new Date().toLocaleString()}"`
+    })
+    res.write(data)
+    res.end()
+    cb ? cb() : {}
+  }
+  next()
 }
